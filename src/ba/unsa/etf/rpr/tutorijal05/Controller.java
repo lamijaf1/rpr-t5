75149e7ca84484f1e34cd02f383d5483c3718251
Lamija <lfazlija1@etf.unsa.ba>
2018-11-12 00:24:03 +0100

    popravljen dio za metode

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,29 +1,15 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-import javafx.beans.property.SimpleStringProperty;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 
 
 public class Controller {
-
-
     public Label display;
-
-
-
- @FXML
-    private SimpleStringProperty tekst;
-    public Controller (){ //dodali smo konstruktor bez parametara
-        tekst=new SimpleStringProperty("0");
-    }
-
-    public void zeroClick(ActionEvent actionEvent) {
-    }
-    public void oneClick(ActionEvent actionEvent ){
-    }
+    public void zeroClick(ActionEvent actionEvent) {}
+    public void oneClick(ActionEvent actionEvent ){}
     public void twoClick(ActionEvent actionEvent ){}
     public void threeClick(ActionEvent actionEvent ){}
     public void fourClick(ActionEvent actionEvent ){}
