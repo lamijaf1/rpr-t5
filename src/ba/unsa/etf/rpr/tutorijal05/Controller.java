46826b81066cb2ad94e02af33f811eb125d3c4c1
Lamija <lfazlija1@etf.unsa.ba>
2018-11-12 20:11:42 +0100

    napravljeno za nulu, 2AT prosla

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,14 +1,47 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 
-
 public class Controller {
+
+    boolean decimalni=false;
+    int cifra;
+    double broj1;
+    int intVr=0;
+    double doubleVr=0;
+    String trenutniBroj="";
     public Label display;
-    public void zeroClick(ActionEvent actionEvent) {}
+    boolean kucano=false;
+    private SimpleStringProperty tekst;
+    public Button btn0;
+
+
+    public Controller() {
+        tekst = new SimpleStringProperty("0");
+    }
+    public String getTekst() {
+        return tekst.get();
+    }
+    public SimpleStringProperty tekstProperty() {
+        return tekst;
+    }
+    public void setTekst(String tekst) {
+        this.tekst.set(tekst);
+    }
+    public void zeroClick(ActionEvent actionEvent) {
+        if (tekst.get().equals("0") && tekst.get().length() == 1) {
+            tekst.set(btn0.getText());
+        } else {
+            tekst.set(tekst.get() + btn0.getText());
+
+        }
+
+    }
     public void oneClick(ActionEvent actionEvent ){}
     public void twoClick(ActionEvent actionEvent ){}
     public void threeClick(ActionEvent actionEvent ){}
@@ -18,4 +51,16 @@ public class Controller {
     public void sevenClick(ActionEvent actionEvent){}
     public void eightClick(ActionEvent actionEvent ){}
     public void nineClick(ActionEvent actionEvent ){}
+    public void dotBtn(ActionEvent actionEvent){
+        decimalni=true;
+    }
+    void DodajCifru(int broj){
+        trenutniBroj+=(""+broj);
+        if(!decimalni) doubleVr = Double.parseDouble(trenutniBroj);
+        else doubleVr=Double.parseDouble(trenutniBroj);
+        display.setText(trenutniBroj);
+        kucano=true;
+    }
+
+
 }
